a61c83a52d4468a67b93d71631f61ceb1f761366
Linus Andreasson <lmmandreasson@gmail.com>
2025-09-22 12:04:11 +0200

    basic functionality added in main

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,36 @@
 package com.example;
 
 import com.example.api.ElpriserAPI;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.io.IOException;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 public class Main { //WIP
+
+
     public static void main(String[] args) {
+
         ElpriserAPI elpriserAPI = new ElpriserAPI();
+        ElpriserAPI.Prisklass prisklass = ElpriserAPI.Prisklass.SE1;
+        List<ElpriserAPI.Elpris> elpriser = getCurrentPrices(elpriserAPI, prisklass);
+
+        System.out.println(elpriser);
     }
+
+    private static List<ElpriserAPI.Elpris> getCurrentPrices(ElpriserAPI elpriserAPI, ElpriserAPI.Prisklass prisklass) {
+        LocalDate idag = LocalDate.now();
+        return getPrices(elpriserAPI, prisklass, idag);
+    }
+
+    private static List<ElpriserAPI.Elpris> getPrices(ElpriserAPI elpriserAPI, ElpriserAPI.Prisklass prisklass, LocalDate datum) {
+        return elpriserAPI.getPriser(datum, prisklass);
+    }
+
+
+
+
 }
